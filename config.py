8f4c5d9a74cb6b79999a0e9775e95fb1a7525158
ChauDN1 <mr.do036@gmail.com>
2023-03-05 21:01:26 +0700

    init project

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+
+# Connect to the database
+
+
+# TODO IMPLEMENT DATABASE URL
+SQLALCHEMY_DATABASE_URI = 'postgresql://chaudo@localhost:5432/chaudn1'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
