38577076e489d31a0087f1a0934b801efb6f31b7
ChauDN1 <mr.do036@gmail.com>
2023-03-10 15:05:05 +0700

    delete venue

--- app.py
+++ app.py
@@ -206,14 +206,31 @@ def create_venue_form():
 
 @app.route('/venues/create', methods=['POST'])
 def create_venue_submission():
-    # TODO: insert form data as a new Venue record in the db, instead
-    # TODO: modify data to be the data object returned from db insertion
+    try:
+        new_venue = Venue(
+            name=request.form["name"],
+            city=request.form["city"],
+            state=request.form["state"],
+            address=request.form["address"],
+            phone=request.form["phone"],
+            genres=request.form.getlist("genres"),
+            image_link=request.form["image_link"],
+            facebook_link=request.form["facebook_link"],
+            website=request.form["website_link"],
+            seeking_talent=True if "seeking_talent" in request.form else False,
+            seeking_description=request.form["seeking_description"],
+        )
+        db.session.add(new_venue)
+        db.session.commit()
+        flash('Venue ' + request.form['name'] + ' was successfully listed!')
+    except Exception as exception:
+        db.session.rollback()
+        print(exception)
+        flash('An error occurred. Venue ' +
+              request.form['name'] + ' could not be listed.')
+    finally:
+        db.session.close()
 
-    # on successful db insert, flash success
-    flash('Venue ' + request.form['name'] + ' was successfully listed!')
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Venue ' + data.name + ' could not be listed.')
-    # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
     return render_template('pages/home.html')
 
 
@@ -224,7 +241,20 @@ def delete_venue(venue_id):
 
     # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
     # clicking that button delete it from the db then redirect the user to the homepage
-    return None
+    try:
+        # delete venue with the given venue_id
+        venue = Venue.query.filter_by(id=venue_id).delete()
+        db.session.commit()
+# flash success message
+        flash('Venue was successfully deleted!')
+    except:
+        # rollback changes if there's an error
+        db.session.rollback()
+    # flash error message
+        flash('An error occurred. Venue could not be deleted.')
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
 
 #  Artists
 #  ----------------------------------------------------------------
