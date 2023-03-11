99dd80b6dabbc72ced65fa6ea17244bdad31f3d9
ChauDN1 <mr.do036@gmail.com>
2023-03-11 14:50:20 +0700

    delete edit

--- app.py
+++ app.py
@@ -70,15 +70,14 @@ class Artist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     website = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String))
     seeking_description = db.Column(db.String(500))
     seeking_venue = db.Column(db.Boolean)
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String))
 # ----------------------------------------------------------------------------#
 # Filters.
 # ----------------------------------------------------------------------------#
